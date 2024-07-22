7ada95f03b96173ac1d5a04e270d9ee1f47892e0
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-07-22 12:57:33 +0000

    rebuild from bot

--- bot/plugins/hosting/caprover.py
+++ bot/plugins/hosting/caprover.py
@@ -1,22 +1,12 @@
-from caprover_api import caprover_api
 import os
 from pyrogram import Client, filters
 from bot.core import filters as fltr
-
-cap = caprover_api.CaproverAPI(
-    dashboard_url= os.environ['cap_url'],
-    password= os.environ['cap_password']
-)
-
-def redeploy_app_via_api(app_name):
-    cap.deploy_app(app_name)
-    print(f"App {app_name} redeployed successfully.")
-
-# Usage
+from bot import logger
+import requests
 
 
-@Client.on_message(filters.command("deploy") & fltr.group("admin"))
-async def set_log_level(client, message):
-    print("redeploying")
-    redeploy_app_via_api(os.environ['cap_app_name'])
+@Client.on_message(filters.command("deploy", "build") & fltr.group("admin"))
+async def build(client, message):
+    logger.info("redeploying")
+    requests.post(os.environ['caprover_build_token'])
 
