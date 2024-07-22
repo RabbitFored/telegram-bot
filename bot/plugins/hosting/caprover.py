d978488c4cd7d3b8d2f4cb5c2ddc7f8fd79264f6
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-07-22 13:00:50 +0000

    update

--- bot/plugins/hosting/caprover.py
+++ bot/plugins/hosting/caprover.py
@@ -8,4 +8,6 @@ import requests
 @Client.on_message(filters.command("deploy", "build") & fltr.group("admin"))
 async def build(client, message):
     logger.info("redeploying")
-    requests.post(os.environ['caprover_build_token'])
+    build_token = os.environ.get("caprover_build_token","")
+    build_url = f"https://captain.theostrich.eu.org/api/v2/user/apps/webhooks/triggerbuild?namespace=captain&token={build_token}"
+    requests.post(build_url)
