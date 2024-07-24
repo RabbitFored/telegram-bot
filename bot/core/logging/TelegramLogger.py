0a36fcc038f4a5fab03826ec7ab6b12cb3547332
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-07-24 08:02:48 +0000

    Log threads

--- bot/core/logging/TelegramLogger.py
+++ bot/core/logging/TelegramLogger.py
@@ -5,26 +5,30 @@ from ..shared import CONFIG
 
 class TelegramHandler(logging.Handler):
 
-    def __init__(self, chat_id, level=logging.NOTSET):
+    def __init__(self, chat_id, thread_id=None, level=logging.NOTSET):
         super().__init__(level)
         self.chat_id = chat_id
-
+        self.thread_id = thread_id
+        
     def emit(self, record):
         log_entry = self.format(record)
         logs = utils.chunkstring(log_entry, 4000)
         for log in logs:
-            utils.botapi(
-            "sendMessage", {
+            data = {
                 "chat_id": self.chat_id,
                 "text": f"<code>{log}</code>",
-                "message_thread_id": 2,
                 "parse_mode": "html"
-            })
+            }
+            if self.thread_id:
+                data["message_thread_id"] = self.thread_id
+            utils.botapi("sendMessage", data)
+            
 
 
 LOG_CHANNEL = CONFIG.settings.get("LOG_CHANNEL", None)
+LOG_THREAD = CONFIG.settings.get("LOG_THREAD", None)
 if LOG_CHANNEL:
-    tgHandler = TelegramHandler(chat_id=LOG_CHANNEL)
+    tgHandler = TelegramHandler(chat_id=LOG_CHANNEL,thread_id=LOG_THREAD)
     tgHandler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
     tgHandler.setFormatter(formatter)
