cec312291ee5fe7cd79c125e9c3ab403d94755fe
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-09-23 08:18:46 +0000

    update

--- bot/plugins/commons.py
+++ bot/plugins/commons.py
@@ -3,8 +3,18 @@ from pyrogram import Client, filters
 from bot import CONFIG, strings
 from bot.core import filters as fltr
 from bot.core.utils import generate_keyboard
+from bot.core import database as db
 
-
+@Client.on_message(filters.private & ~ filters.me , group=-100)
+async def user_load(client, message):
+    userID = message.from_user.id
+    user = await db.get_user(userID, fetch_info=True)
+    if not user:
+        await db.add_user(message)
+    else:
+        await user.refresh(message)
+    message.continue_propagation()
+    
 @Client.on_message(fltr.cmd(["start"]))
 async def start(client, message):
     text = strings.get("start_txt", user=message.from_user.mention)
